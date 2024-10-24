0dbc2269dc2f58d1dda5ac8b85587a58d42741f2
saran-cov <sarang@covalsys.com>
2024-10-24 16:40:14 +0530

    Chnage new

--- app/src/main/java/com/covalsys/phi_scanner/ui/stock_count_history/StockHistoryFragment.java
+++ app/src/main/java/com/covalsys/phi_scanner/ui/stock_count_history/StockHistoryFragment.java
@@ -120,6 +120,11 @@ public class StockHistoryFragment extends BaseFragment<StockHistoryViewModel, St
             showSnackBar(msg);
     }
 
+    /*@Override
+    public void onPostSuccess(String msg) {
+        showSnackBar(msg);
+    }*/
+
     @Override
     public void onPostFailed(String msg) {
          showSnackBar(msg);
