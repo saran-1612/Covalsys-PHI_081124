982142fb17c637eba739e99522989b3aa4e2c1fe
saran-cov <sarang@covalsys.com>
2024-10-28 12:08:01 +0530

    Changes Req

--- app/src/main/java/com/covalsys/phi_scanner/ui/stock_count_history/StockHistoryFragment.java
+++ app/src/main/java/com/covalsys/phi_scanner/ui/stock_count_history/StockHistoryFragment.java
@@ -114,26 +114,11 @@ public class StockHistoryFragment extends BaseFragment<StockHistoryViewModel, St
         historyAdapter.addCustomers(responseObjectList);
         historyAdapter.notifyDataSetChanged();
     }
-    /*private void setBatchList(List<StockHistoryModel.ResponseObject> responseObjectList){
-        historyAdapter.clearList();
-        historyAdapter.addCustomers(responseObjectList);
-        historyAdapter.notifyDataSetChanged();
-    }*/
 
     @Override
     public void onPostSuccess(String msg) {
             showSnackBar(msg);
     }
-
-    /*@Override
-    public void onPostSuccess(String msg) {
-        showSnackBar(msg);
-    }*/
-
-    /*@Override
-    public void onPostSuccess(String msg) {
-        showSnackBar(msg);
-    }*/
 
     @Override
     public void onPostFailed(String msg) {
